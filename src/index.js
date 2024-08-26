71851bd3bff9503ab16a60f4d97d3f402e363c78
Arthur <forceaceflash@gmail.com>
2024-08-26 10:56:46 +0300

    finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let i = 0
+    let result = ''
+    n = Math.abs(n)
+    n = n.toString()
+    while (n.length > i) {
+        result = `${n[i]}${result}`
+        i += 1
+    }
+    return parseInt(result, 10)
 }
+
